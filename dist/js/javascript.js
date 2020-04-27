c55b07af2b262cfb3ddb52fd51fec1d69b8a5fe9
Daniel V.M <Daniel@MacBook-Air-de-Daniel.local>
2020-04-26 20:20:14 -0500

    pendiente loader

--- dist/js/javascript.js
+++ dist/js/javascript.js
@@ -1,88 +1,103 @@
-$(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: 'fade-in',
-    outClass: 'fade-out',
-    inDuration: 1500,
-    outDuration: 800,
-    linkElement: '.animsition-link',
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: ['animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay: false,
-    overlayClass: 'animsition-overlay-slide',
-    overlayParentElement: 'body',
-    transition: function(url) {
-      window.location.href = url;
-    }
-  });
-});
+ var firstVar;
+ var secondVar;
 
-$(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: 'fade-in-up',
-    outClass: 'fade-out-up',
-    inDuration: 1500,
-    outDuration: 800,
-    linkElement: '.animsition-link',
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: ['animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay: false,
-    overlayClass: 'animsition-overlay-slide',
-    overlayParentElement: 'body',
-    transition: function(url) {
-      window.location.href = url;
-    }
-  });
-});
+ function firstFunction() {
+   firstVar = setTimeout(showLoader, 2000);
+   secondVar = setTimeout(showPage, 4000);
+ }
 
-$(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: 'fade-in-right',
-    outClass: 'fade-out-right',
-    inDuration: 1500,
-    outDuration: 800,
-    linkElement: '.animsition-link',
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: ['animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay: false,
-    overlayClass: 'animsition-overlay-slide',
-    overlayParentElement: 'body',
-    transition: function(url) {
-      window.location.href = url;
-    }
-  });
-});
+ function showLoader() {
+   document.getElementById("start").style.display = "none";
+   document.getElementById("loader").style.display = "block";
+ }
 
-var back = document.getElementById('back');
-back.addEventListener("click", function() {
-  window.history.back();
-}, false);
+ function showPage() {
+   document.getElementById("loader").style.display = "none";
+   document.getElementById("onboarding").style.display = "block";
+ }
 
+ $(document).ready(function () {
+   $(".animsition").animsition({
+     inClass: 'fade-in',
+     outClass: 'fade-out',
+     inDuration: 1500,
+     outDuration: 800,
+     linkElement: '.animsition-link',
+     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+     loading: true,
+     loadingParentElement: 'body', //animsition wrapper element
+     loadingClass: 'animsition-loading',
+     loadingInner: '', // e.g '<img src="loading.svg" />'
+     timeout: false,
+     timeoutCountdown: 5000,
+     onLoadEvent: true,
+     browser: ['animation-duration', '-webkit-animation-duration'],
+     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+     overlay: false,
+     overlayClass: 'animsition-overlay-slide',
+     overlayParentElement: 'body',
+     transition: function (url) {
+       window.location.href = url;
+     }
+   });
+ });
 
+ $(document).ready(function () {
+   $(".animsition").animsition({
+     inClass: 'fade-in-up',
+     outClass: 'fade-out-up',
+     inDuration: 1500,
+     outDuration: 800,
+     linkElement: '.animsition-link',
+     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+     loading: true,
+     loadingParentElement: 'body', //animsition wrapper element
+     loadingClass: 'animsition-loading',
+     loadingInner: '', // e.g '<img src="loading.svg" />'
+     timeout: false,
+     timeoutCountdown: 5000,
+     onLoadEvent: true,
+     browser: ['animation-duration', '-webkit-animation-duration'],
+     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+     overlay: false,
+     overlayClass: 'animsition-overlay-slide',
+     overlayParentElement: 'body',
+     transition: function (url) {
+       window.location.href = url;
+     }
+   });
+ });
 
+ $(document).ready(function () {
+   $(".animsition").animsition({
+     inClass: 'fade-in-right',
+     outClass: 'fade-out-right',
+     inDuration: 1500,
+     outDuration: 800,
+     linkElement: '.animsition-link',
+     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+     loading: true,
+     loadingParentElement: 'body', //animsition wrapper element
+     loadingClass: 'animsition-loading',
+     loadingInner: '', // e.g '<img src="loading.svg" />'
+     timeout: false,
+     timeoutCountdown: 5000,
+     onLoadEvent: true,
+     browser: ['animation-duration', '-webkit-animation-duration'],
+     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+     overlay: false,
+     overlayClass: 'animsition-overlay-slide',
+     overlayParentElement: 'body',
+     transition: function (url) {
+       window.location.href = url;
+     }
+   });
+ });
+
+ var back = document.getElementById('back');
+ back.addEventListener("click", function () {
+   window.history.back();
+ }, false);
