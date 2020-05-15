38a9515884d8f1ab1fba90fc70962df6b704c353
Daniel Valderrama <dny9919@gmail.com>
2020-05-15 18:35:36 -0500

    falta agregar datos a directorio y enlaces

--- dist/js/javascript.js
+++ dist/js/javascript.js
@@ -16,6 +16,8 @@
     document.getElementById("onboarding").style.display = "block";
   }
 
+  
+
  $(document).ready(function () {
    $(".animsition").animsition({
      inClass: 'fade-in',
@@ -96,6 +98,51 @@
      }
    });
  });
+ var consulta = $("#searchTable").DataTable();
+ $("#inputBusqueda").keyup(function () {
+   consulta.search($(this).val()).draw();
+
+   $("#Datos").css({
+     "height": "100vh",
+     "background": "rgba(0,0,0,0.1)"
+   })
+  
+   if ($("#inputBusqueda").val() == "") {
+     $("#Datos").css({
+       "height": "auto",
+       "background": "none"
+     })
+     $("#search").hide("fast")
+     
+   } else {
+     $("#search").fadeIn("fast");
+   }
+
+ })
+ var ocultar = document.getElementById('search')
+ ocultar.addEventListener("click", function () {
+
+   if (true) {
+   
+     $("#search").hide("fast")
+      $("#Datos").css({
+        "height": "0%",
+        "background": "none"
+      })
+   } 
+ })
+ var ocultar2 = document.getElementById('inputBusqueda')
+ ocultar2.addEventListener("click", function () {
+
+   if (true) {
+
+     $("#search").hide("fast")
+     $("#Datos").css({
+       "height": "0%",
+       "background": "none"
+     })
+   }
+ })
 
  var back = document.getElementById('back');
  back.addEventListener("click", function () {
