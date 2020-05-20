cd5edded09d68a788e8a7b17725b50092ff32145
Daniel Valderrama <dny9919@gmail.com>
2020-05-20 01:25:10 -0500

    update header, add top-button directorio

--- dist/js/search.js
+++ dist/js/search.js
@@ -1,11 +1,29 @@
- var consulta = $("#searchTable").DataTable();
+ window.onscroll = function () { 
+  if (document.documentElement.scrollTop > 120) {
+    document.querySelector('.top-container')
+      .classList.add('show');
+  } else {
+    document.querySelector('.top-container')
+      .classList.remove('show');
+  }
+}
+
+document.querySelector('.top-container')
+  .addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+var consulta = $("#searchTable").DataTable();
  $("#inputBusqueda").keyup(function () {
    consulta.search($(this).val()).draw();
 
    $("#Datos").css({
-     "height": "100vh",
+     "height": "100%",
      "background": "rgba(0,0,0,0.1)"
    })
+
 
    if ($("#inputBusqueda").val() == "") {
      $("#Datos").css({
@@ -26,7 +44,6 @@
 
      $("#search").hide("fast")
      $("#Datos").css({
-       "height": "0%",
        "background": "none"
      })
    }
@@ -38,8 +55,8 @@
 
      $("#search").hide("fast")
      $("#Datos").css({
-       "height": "0%",
        "background": "none"
      })
    }
  })
+
